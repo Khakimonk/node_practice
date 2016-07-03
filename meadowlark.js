195626781f3b15459e33a64643799dc87ced73a0
khakimonk <khakimonk@gmail.com>
2016-07-04 00:18:52 +0100

    finished qa

--- meadowlark.js
+++ meadowlark.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
-//set up handlebars view engine
-var handlebars = require('express-handlebars')
-    .create({
-        defaultLayout: 'main'
-    });
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -14,27 +21,46 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(reg, res) {
-    res.render('home');
+// set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
+	next();
 });
 
-app.get('/about', function(req, res) {
-    res.render('about');
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/about', function(req,res){
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
+});
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
-//custom 404 page
-app.use(function(req, res) {
-    res.status(404);
-    res.render('404');
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
 });
 
-//custom 500 page
-app.use(function(err, req, res, next) {
-    console.log(err.stack);
-    res.status(500);
-    res.render('500');
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
